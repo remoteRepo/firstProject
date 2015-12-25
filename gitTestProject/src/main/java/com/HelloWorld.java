18daa9fcd2e73ba8f687e5d6aac9957a9fba350d
TestUser <test_git@mail.ru>
2015-12-25 20:59:07 +0200

    second commit in master branch

--- gitTestProject/src/main/java/com/HelloWorld.java
+++ gitTestProject/src/main/java/com/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world");
 		System.out.println("first commit in master branch");
+		System.out.println("second commit in master branch");
     }
 }
