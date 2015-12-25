40da06b332fe662211cfc23cbdaa3a0e24580ab0
TestUser <test_git@mail.ru>
2015-12-25 20:38:17 +0200

    first test in master branch

--- gitTestProject/src/main/java/com/HelloWorld.java
+++ gitTestProject/src/main/java/com/HelloWorld.java
@@ -3,5 +3,6 @@ package com;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world");
+		System.out.println("first commit in master branch");
     }
 }
